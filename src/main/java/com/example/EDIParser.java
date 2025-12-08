acf92920871544c1043337880bb0b5687081d74f
Deepak Mallya <deepakmk@gmail.com>
2025-12-08 20:54:20 +1100

    Added EDI parser

---
+++ src/main/java/com/example/EDIParser.java
@@ -0,0 +1,64 @@
+package com.example;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.File;
+
+import io.xlate.edi.stream.EDIInputFactory;
+import io.xlate.edi.stream.EDIStreamReader;
+
+public class EDIParser {
+    public static void main(String[] args) throws Exception {
+        EDIInputFactory factory = EDIInputFactory.newFactory();
+
+        // Obtain Stream to the EDI document to read.
+        File file = new File("/Users/user/sourcecode/GitHub/mq-edifact-json/src/main/resources/nevdis.edi");
+        InputStream stream = new FileInputStream(file);
+
+        EDIStreamReader reader = factory.createEDIStreamReader(stream);
+
+        while (reader.hasNext()) {
+            switch (reader.next()) {
+                case START_INTERCHANGE:
+                    /* Retrieve the standard - "X12", "EDIFACT", or "TRADACOMS" */
+                    String standard = reader.getStandard();
+
+                    /*
+                     * Retrieve the version string array. An array is used to support
+                     * the componentized version element used in the EDIFACT standard.
+                     *
+                     * e.g. [ "00501" ] (X12) or [ "UNOA", "3" ] (EDIFACT)
+                     */
+                    String[] version = reader.getVersion();
+                    break;
+
+                case START_SEGMENT:
+                    // Retrieve the segment name - e.g. "ISA" (X12), "UNB" (EDIFACT), or "STX"
+                    // (TRADACOMS)
+                    String segmentName = reader.getText();
+                    System.out.println(segmentName);
+                    break;
+
+                case END_SEGMENT:
+                    break;
+
+                case START_COMPOSITE:
+                    break;
+
+                case END_COMPOSITE:
+                    break;
+
+                case ELEMENT_DATA:
+                    // Retrieve the value of the current element
+                    String data = reader.getText();
+                    System.out.println(data);
+                    break;
+            }
+        }
+
+        reader.close();
+        stream.close();
+
+    }
+
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             RabbitMQClient client = new RabbitMQClient();
-            String message = "UNH+1+ORDERS:D:96A:UN:EAN008'BGM+220+12345+9'DTM+137:20240815:102'NAD+BY+5412345000013::9'NAD+SU+4012345000094LIN+1++4000862141404:EN'PIA+1+ABC123:SA'QTY+21:500'PRI+AAA:800'UNT+9+1'";
+            String message = "UNA:+.?*'UNB+UNOA:4+5412345678908:14+8798765432106:14+20020102:1015+INT12346’UNH+1+ORDERS:D:96A:UN:EAN008'BGM+220+12345+9'DTM+137:20240815:102'NAD+BY+5412345000013::9'NAD+SU+4012345000094LIN+1++4000862141404:EN'PIA+1+ABC123:SA'QTY+21:500'PRI+AAA:800'UNT+9+1'";
             // if (args.length > 0) {
             // message = args[0];
             // }
